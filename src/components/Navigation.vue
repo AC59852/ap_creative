c0efe96138572d05ad582fbc8aca3f2a6ce751ba
Austin Caron <austin.caron1@gmail.com>
2021-04-17 17:11:43 -0400

    added ipad horiztonal scaling

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,17 +1,17 @@
 <template>
   <header>
       <nav id="topNav">
-          <img src="img/svgs/menu.svg" alt="dropdown menu icon" @click="showMenu()">
+          <div class="iconCon">
+            <router-link to="/"><img src="img/svgs/AP_logo_dark.svg" alt="logo"></router-link>
+            <img @click="showMenu()" src="img/svgs/info.svg" alt="search icon">
+          </div>
           <ul>
               <span @click="showMenu()">X</span>
               <li @click="showMenu()"><router-link to="/">Home</router-link></li>
               <li @click="showMenu()"><router-link to="/about">About</router-link></li>
               <li @click="showMenu()"><router-link to="/contact">Contact</router-link></li>
           </ul>
-        <div class="iconCon">
-          <router-link to="/"><img src="img/svgs/AP_logo_dark.svg" alt="logo"></router-link>
-          <img @click="showMenu()" src="img/svgs/info.svg" alt="search icon">
-      </div>
+          <img src="img/svgs/menu.svg" alt="dropdown menu icon" @click="showMenu()">
       </nav>
   </header>
 </template>
